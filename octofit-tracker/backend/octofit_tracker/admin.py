55544066bb945ce48c11decd056368ca07c8c510
sanju139 <60957202+sanju139@users.noreply.github.com>
2026-03-23 11:10:59 +0000

    db creation and configuration changes

---
+++ octofit-tracker/backend/octofit_tracker/admin.py
@@ -0,0 +1,9 @@
+# Register models for admin site
+from django.contrib import admin
+from .models import User, Team, Workout, Activity, Leaderboard
+
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Workout)
+admin.site.register(Activity)
+admin.site.register(Leaderboard)
